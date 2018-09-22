6b550c8c1b909552df6e6e5fa1fc155f5acd0a0a
Benjamin Morrison <morrisonb84@gmail.com>
2018-09-22 15:46:41 -0400

    added collision check / health tracking to bird

--- ToriNoKuso/source/Bird.hx
+++ ToriNoKuso/source/Bird.hx
@@ -5,15 +5,6 @@ import flixel.FlxG;
 
 class Bird extends FlxSprite {
 
-  //public var x:Int;
-  //public var y:Int;
-
-  public var speed:Float = 200;
-  //public var _up:Bool = false;
-  //public var _down:Bool = false;
-  //public var _left:Bool = false;
-  //public var _right:Bool = false;
-
 
   public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset)
 	{
@@ -22,22 +13,24 @@ class Bird extends FlxSprite {
 
 		loadGraphic("assets/images/squirrels1.png", true, 16, 16);
 
+    var health:Int = 5;
+    var dead:Bool = false;
+    //var ammo:Ammo = 0;
+
 	}
 
-	public function movement():Void
-  {
-    _up = FlxG.keys.anyPressed([UP, W]);
-    _down = FlxG.keys.anyPressed([DOWN, S]);
-    _left = FlxG.keys.anyPressed([LEFT, A]);
-    _right = FlxG.keys.anyPressed([RIGHT, D]);
-
-    // cancel out opposing directions
-    if (_up && _down){
-     _up = _down = false;
+  public function healthTracker() {
+    if (health > 0) {
+      health--;
+      trace("Player health now at ", health);
     }
-    if (_left && _right){
-     _left = _right = false;
-    }
-
+    dead = true;
+    trace("Player died");
+    //game over, screen freezes and text appears
   }
+
+  pubic function diveForFood() {
+    //on button press, move player vertically down the screen
+  }
+
 }
--- ToriNoKuso/source/PlayState.hx
+++ ToriNoKuso/source/PlayState.hx
@@ -4,25 +4,58 @@ import flixel.FlxState;
 import flixel.util.FlxTimer;
 import flixel.FlxG;
 import flixel.math.FlxRandom;
+import flixel.FlxSprite;
+import flixel.FlxObject;
+import flixel.util.FlxCollision;
+import flixel.group.FlxGroup.FlxTypedGroup;
 
 class PlayState extends FlxState
 {
 
 	var _player:Bird;
+	var _sqgroup:FlxTypedGroup<Squirrel>;
 	override public function create():Void
 	{
-		
+
 		new FlxTimer().start(2, spawnSQ, 0);
+
+		_sqgroup = new FlxTypedGroup<Squirrel>(0);
 
 		_player = new Bird(50,50);
 		add(_player);
 
 		super.create();
+		trace(FlxG.width, FlxG.height);
 
 	}
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
+		playerMovement();
+		collisionCheck();
+
+	}
+
+
+	public function spawnSQ(Timer:FlxTimer):Void
+	{
+
+		var _ran:FlxRandom = new FlxRandom();
+		var _rSpawn:Int = Std.int(_ran.float(1, 5));
+
+		for (i in 0..._rSpawn )
+		{
+			var _rNum:Int = Std.int(_ran.float(2, 10));
+			var _rNum2:Int = Std.int(_ran.float(1, 5));
+
+			var _sq:Squirrel = new Squirrel(FlxG.width + 10 , _rNum * 20, 1, _rNum2 );
+			add(_sq);
+			_sqgroup.add(_sq);
+			_sq.move(_player);
+		}
+	}
+
+	public function playerMovement(){
 
 		if(FlxG.keys.pressed.LEFT || FlxG.keys.pressed.A) {
 			_player.x-=2;
@@ -36,23 +69,18 @@ class PlayState extends FlxState
 		if(FlxG.keys.pressed.DOWN || FlxG.keys.pressed.S) {
 			_player.y+=2;
 		}
+
+		for (members in _sqgroup)
+		{
+			members.checkdead();
+		}
 	}
 
-
-	public function spawnSQ(Timer:FlxTimer):Void
-	{
-
-		var _ran:FlxRandom = new FlxRandom();
-		var _rSpawn:Int = Std.int(_ran.float(1, 5));
-		trace("new");
-		for (i in 0..._rSpawn )
-		{
-			var _rNum:Int = Std.int(_ran.float(2, 10));
-			var _rNum2:Int = Std.int(_ran.float(1, 5));
-			trace(_rNum2);
-			var _sq:Squirrel = new Squirrel(FlxG.width + 10 , _rNum * 20, 1, _rNum2 );
-			add(_sq);
-			_sq.move(_player);
+	//need to add delay so that there is a couple seconds of "invincibility" after
+	//the first instance of a collision
+	public function collisionCheck(){
+		if(FlxG.overlap(_player, _sqgroup)) {
+			_player.healthTracker();
 		}
 	}
 
