0b15519e8639030bc77f1812f6ecd6031ccece19
minpark283 <parkm4@rpi.edu>
2018-09-25 22:35:29 -0400

    Added win and intro scene

---
+++ ToriNoKuso/source/WinState.hx
@@ -0,0 +1,23 @@
+package;
+import flixel.FlxState;
+
+/**
+ * ...
+ * @author ...
+ */
+class WinState extends FlxState
+{
+
+	
+	override public function create():Void
+	{
+	
+	}
+	
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+	
+	
+}
